94062c54044d6adda8dc3de27192c56fb3a55a8f
Konstantin <sqb@mail.ru>
2016-09-06 17:32:33 +0300

    Game flow updated

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,76 +5,12 @@ namespace ConstantineSpace.SimpleUI
 {
     public class GameManager : Singleton<GameManager>
     {
-        [Header("Screens")]
-        public GameObject StartScreen;
-        public GameObject GameScreen;
-        public GameObject PauseScreen;
-        public GameObject GameOverScreen;
-        public GameObject WinLevelScreen;
-
-
         /// <summary>
         /// Initialization.
         /// </summary>
-        public void Start()
+        private void Start()
         {
-            GuiManager.Instance.SetScreenActive(StartScreen);
-        }
-
-        /// <summary>
-		/// Sets the Game screen.
-		/// </summary>
-		/// <param name="currentScreen"> The current screen game object that will be inactive.</param>
-        public virtual void GoToGame(GameObject currentScreen)
-        {
-            GuiManager.Instance.SetScreenActive(currentScreen, GameScreen);
-        }
-
-        /// <summary>
-        /// Sets the Start screen.
-        /// </summary>
-        /// <param name="currentScreen"> The current screen game object that will be inactive.</param>
-        public virtual void GoToStart(GameObject currentScreen)
-        {
-            GuiManager.Instance.SetScreenActive(currentScreen, StartScreen);
-            GuiManager.Instance.SetScreenInActive(GameScreen);
-        }
-
-        /// <summary>
-        /// Pauses the game.
-        /// </summary>
-        public virtual void Pause()
-        {
-            if (!PauseScreen.activeInHierarchy)
-            {
-                GuiManager.Instance.SetScreenActive(PauseScreen);
-            }
-        }
-
-        /// <summary>
-        /// Unpauses the game.
-        /// </summary>
-        public virtual void UnPause()
-        {
-            Debug.Log("UnPause");
-            GuiManager.Instance.SetScreenInActive(PauseScreen);
-        }
-
-        /// <summary>
-        /// Restarts the game.
-        /// </summary>
-        public virtual void Restart()
-        {
-            Debug.Log("Restart");
-            GuiManager.Instance.SetScreenInActive(PauseScreen);
-        }
-
-        /// <summary>
-        /// Sets the Win screen.
-        /// </summary>
-        public virtual void WinGame()
-        {
-            GuiManager.Instance.SetScreenActive(WinLevelScreen);
+           ScreenManager.Instance.GoToStart(null);
         }
     }
 }
---
+++ Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,90 @@
+﻿using ConstantineSpace.Tools;
+using UnityEngine;
+
+namespace ConstantineSpace.SimpleUI
+{
+    public class ScreenManager : Singleton<ScreenManager>
+    {
+        [Header("Screens")]
+        [SerializeField]
+        private GameObject _startScreen;
+        [SerializeField]
+        private GameObject _gameScreen;
+        [SerializeField]
+        private GameObject _pauseScreen;
+        [SerializeField]
+        private GameObject _gameOverScreen;
+        [SerializeField]
+        private GameObject _winLevelScreen;
+
+
+        /// <summary>
+		/// Sets the Game screen.
+		/// </summary>
+		/// <param name="currentScreen"> The current screen game object that will be inactive.</param>
+        public virtual void GoToGame(GameObject currentScreen)
+        {
+            GuiManager.Instance.SetScreenActive(currentScreen, _gameScreen);
+        }
+
+        /// <summary>
+        /// Sets the Start screen.
+        /// </summary>
+        /// <param name="currentScreen"> The current screen game object that will be inactive.</param>
+        public virtual void GoToStart(GameObject currentScreen)
+        {
+            if (currentScreen != null)
+            {
+                GuiManager.Instance.SetScreenInActive(currentScreen);
+                GuiManager.Instance.SetScreenInActive(_gameScreen);
+            }
+            GuiManager.Instance.SetScreenActive(_startScreen);
+        }
+
+        /// <summary>
+        /// Pauses the game.
+        /// </summary>
+        public virtual void Pause()
+        {
+            if (!_pauseScreen.activeInHierarchy)
+            {
+                GuiManager.Instance.SetScreenActive(_pauseScreen);
+            }
+        }
+
+        /// <summary>
+        /// Unpauses the game.
+        /// </summary>
+        public virtual void UnPause()
+        {
+            Debug.Log("UnPause");
+            GuiManager.Instance.SetScreenInActive(_pauseScreen);
+        }
+
+        /// <summary>
+        /// Restarts the game.
+        /// </summary>
+        /// <param name="currentScreen"> The current screen game object that will be inactive.</param>
+        public virtual void Restart(GameObject currentScreen)
+        {
+            Debug.Log("Restart");
+            GuiManager.Instance.SetScreenInActive(currentScreen);
+        }
+
+        /// <summary>
+        /// Sets the Win screen.
+        /// </summary>
+        public virtual void WinGame()
+        {
+            GuiManager.Instance.SetScreenActive(_winLevelScreen);
+        }
+
+        /// <summary>
+        /// Sets the GameOver screen.
+        /// </summary>
+        public virtual void GameOver()
+        {
+            GuiManager.Instance.SetScreenActive(_gameOverScreen);
+        }
+    }
+}

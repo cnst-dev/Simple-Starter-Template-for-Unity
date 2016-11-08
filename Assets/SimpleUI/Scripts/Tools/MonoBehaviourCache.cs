d18961dd41489a8f6e73ab61f63c50be4f9eeaa4
Konstantin <sqb@mail.ru>
2016-11-08 12:02:28 +0300

    MonoBehaviour caching added

---
+++ Assets/SimpleUI/Scripts/Tools/MonoBehaviourCache.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace ConstantineSpace.SimpleUI
+{
+    /// <summary>
+    ///     Use for caching in MonoBehavior.
+    /// </summary>
+    public abstract class MonoBehaviourCache : MonoBehaviour
+    {
+        protected Transform _cachedTransform;
+        protected GameObject _cachedGameObject;
+        protected Collider2D _cachedCollider;
+
+        public new Transform transform
+        {
+            get
+            {
+                if (_cachedTransform == null)
+                {
+                    _cachedTransform = base.transform;
+                }
+                return _cachedTransform;
+            }
+        }
+
+        public new GameObject gameObject
+        {
+            get
+            {
+                if (_cachedGameObject == null)
+                {
+                    _cachedGameObject = base.gameObject;
+                }
+                return _cachedGameObject;
+            }
+        }
+
+        private void Awake()
+        {
+            if (_cachedTransform == null)
+            {
+                _cachedTransform = base.transform;
+            }
+
+            if (_cachedGameObject == null)
+            {
+                _cachedGameObject = base.gameObject;
+            }
+        }
+    }
+}
